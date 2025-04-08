a2b4c2fa495d714ba71a910e7b81d28b127282f5
greggor <greggor@gmail.com>
2025-04-08 11:26:05 +0200

    Tests reorg and rename for clarity.

--- tests.py
+++ tests.py
@@ -3,15 +3,7 @@ import unittest.mock
 from pathlib import Path
 
 from drafix import add_user_id_document, find_user_data, load_dra_file, save_dra_file, KEDU_5_5_NAMESPACE
-from lxml.etree import ElementTree, XML, tostring as xml_to_string, fromstring as xml_from_string
-from functools import partial
-from dataclasses import dataclass
-
-
-@dataclass
-class NamespaceTestData:
-    namespace: str
-    message: str
+from lxml.etree import XML, tostring as xml_to_string, fromstring as xml_from_string
 
 
 class MyTestCase(unittest.TestCase):
@@ -21,33 +13,25 @@ class MyTestCase(unittest.TestCase):
     xsi:schemaLocation="http://www.zus.pl/2022/KEDU_5_5_kedu 5_5.xsd">
     <ZUSDRA>{user_id}</ZUSDRA></KEDU>'''
 
-    def test_namespace(self):
+    def test_invalid_namespace(self):
         invalid_namespace = 'http://www.zus.pl/2023/KEDU_5_6'
 
-        test_data = [NamespaceTestData(KEDU_5_5_NAMESPACE, 'Malformed ZUS DRA document, '
-                                                           'expected single user id data entry, found: 0!'),
-                     NamespaceTestData(invalid_namespace, f'Actual namespace "{invalid_namespace}" '
-                                                          f'differs than expected one "{KEDU_5_5_NAMESPACE}", '
-                                                          f'document cannot be processed')]
+        error_message = f'Actual namespace "{invalid_namespace}" differs than expected one "{KEDU_5_5_NAMESPACE}", ' \
+                         f'document cannot be processed'
 
-        for test_item in test_data:
-            namespace = test_item.namespace
-            document = XML(self.xml.format(namespace=namespace, user_id=''))
-            try:
-                self.assertRaisesRegex(AssertionError, test_item.message, find_user_data, document, KEDU_5_5_NAMESPACE)
-            except Exception:
-                print(f'{test_item = }')
-                raise
+        document = XML(self.xml.format(namespace=invalid_namespace, user_id=''))
+        self.assertRaisesRegex(AssertionError, error_message, find_user_data, document, KEDU_5_5_NAMESPACE)
 
     def test_malformed_document(self):
         user_id = '<II><p1></p1><p2></p2><p3></p3><p6></p6><p7></p7><p8></p8><p9></p9></II>'
         for i in range(0, 2):
             document = XML(self.xml.format(namespace=KEDU_5_5_NAMESPACE, user_id=(user_id * i)))
-            if i == 1:
-                _ = find_user_data(document, KEDU_5_5_NAMESPACE)
+            if i > 0:
+                entries = find_user_data(document, KEDU_5_5_NAMESPACE)
+                self.assertEqual(len(entries), i)
             else:
-                self.assertRaisesRegex(AssertionError, 'Malformed ZUS DRA document, '
-                                                       f'expected single user id data entry, found: {i}!',
+                self.assertRaisesRegex(AssertionError,
+                                       'Malformed ZUS DRA document, expected at least one user id data entry, found: 0!',
                                        find_user_data,
                                        document,
                                        KEDU_5_5_NAMESPACE)
@@ -59,7 +43,9 @@ class MyTestCase(unittest.TestCase):
                    b'<p1/><p2/><p3/><p4>aaa</p4><p5>bbb</p5><p6/><p7/><p8/><p9/></II>'
         document = XML(self.xml.format(namespace=KEDU_5_5_NAMESPACE, user_id=user_id))
         user_info_entry = find_user_data(document, KEDU_5_5_NAMESPACE)
-        user_info_entry = add_user_id_document(user_info_entry, 'aaa', 'bbb')
+        self.assertIsInstance(user_info_entry, list)
+        self.assertEqual(len(user_info_entry), 1)
+        user_info_entry = add_user_id_document(user_info_entry[0], 'aaa', 'bbb')
         self.assertEqual(xml_to_string(user_info_entry), expected)
 
     def test_load_xml(self):
